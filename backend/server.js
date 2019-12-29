18b3157b0044dce96185005e7860bde4f11e4c4a
uriberg <uribe1927@gmail.com>
2019-12-29 12:41:16 +0200

    'saturday night - another try to fix mongo deployment'

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,25 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', (request, response) => {
-        response.sendFile(path.join(__dirname, 'client/build', 'index.tsx'))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     // Handle React routing, return all requests to React app
+//     app.get('*', (request, response) => {
+//         response.sendFile(path.join(__dirname, 'client/build', 'index.tsx'))
+//     })
+// }
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_URI || process.env.LiveBet_URI;
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+};
+
+// const uri = process.env.MONGO_URI || 'mongodb://uriberg:uriberg2@ds011870.mlab.com:11870/heroku_n0jlwqcz';
+const uri = process.env.LiveBet_URI || 'mongodb://uriberg:uriberg2@ds011870.mlab.com:11870/heroku_n0jlwqcz';
+// const uri = process.env.LiveBet_URI;
 mongoose.connect(uri, {userNewUrlParser: true, useCreateIndex: true});//flags are needed for taking care of MongoDB updates changes
 const connection = mongoose.connection;
 connection.once('open', () => {
