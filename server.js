1962879ec8b1d07920917bb0db2cea12cd45d47d
uriberg <uribe1927@gmail.com>
2020-06-27 05:20:15 +0300

    adding gitignore to backend

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Control-Type, Authorization');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Control-Type, Authorization');
+    next();
+});
 
 
 
