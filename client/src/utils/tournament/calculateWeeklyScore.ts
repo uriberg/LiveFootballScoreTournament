402d1f54abd3af1c0b68483a460974da4aca72c6
Uri_Berger <uribe1927@gmail.com>
2020-07-09 19:01:27 +0300

    *fix weekly score calculation bug

--- client/src/utils/tournament/calculateWeeklyScore.ts
+++ client/src/utils/tournament/calculateWeeklyScore.ts
@@ -1,4 +1,5 @@
 import {MatchType, User} from "../../constants/interfaces";
+import {DEBUG} from "../../constants/settings";
 
 export const calculateWeeklyScore = (stateUsers: any, stateCurrMatches: any, tournamentId: string) => {
     let users: User [] = [...stateUsers];
@@ -13,19 +14,21 @@ export const calculateWeeklyScore = (stateUsers: any, stateCurrMatches: any, tou
                 let tieOddIndex = matches[j].tieOdd.findIndex((item: any) => item.tournamentId === tournamentId);
                 let awayOddIndex = matches[j].awayOdd.findIndex((item: any) => item.tournamentId === tournamentId);
                 if (homeOddIndex > -1 && tieOddIndex > -1 && awayOddIndex > -1) {//i.e odds have been submitted
+                    //DEBUG && console.log('odds have been submitted');
                     if (matches[j].goalsHomeTeam > matches[j].goalsAwayTeam) {
                         // @ts-ignore
-                        if (matches[j].homeWinUsers.findIndex((item: any) => item.name === users[i].name && item.tournamentId === tournamentId) > -1) {
+                        if (matches[j].homeWinUsers.findIndex((item: any) => item.nickname === users[i].nickname && item.tournamentId === tournamentId) > -1) {
+                            DEBUG && console.log('home win is found');
                             users[i].weeklyScore = +parseFloat((users[i].weeklyScore + matches[j].homeOdd[homeOddIndex].value).toFixed(2));
                         }
                     } else if (matches[j].goalsHomeTeam < matches[j].goalsAwayTeam) {
                         // @ts-ignore
-                        if (matches[j].awayWinUsers.findIndex((item: any) => item.name === users[i].name && item.tournamentId === tournamentId) > -1) {
+                        if (matches[j].awayWinUsers.findIndex((item: any) => item.nickname === users[i].nickname && item.tournamentId === tournamentId) > -1) {
                             users[i].weeklyScore = +parseFloat((users[i].weeklyScore + matches[j].awayOdd[awayOddIndex].value).toFixed(2));
                         }
                     } else if (matches[j].goalsHomeTeam === matches[j].goalsAwayTeam) {
                         // @ts-ignore
-                        if (matches[j].tieUsers.findIndex((item: any) => item.name === users[i].name && item.tournamentId === tournamentId) > -1) {
+                        if (matches[j].tieUsers.findIndex((item: any) => item.nickname === users[i].nickname && item.tournamentId === tournamentId) > -1) {
                             users[i].weeklyScore = +parseFloat((users[i].weeklyScore + matches[j].tieOdd[tieOddIndex].value).toFixed(2));
                         }
                     }
@@ -36,6 +39,6 @@ export const calculateWeeklyScore = (stateUsers: any, stateCurrMatches: any, tou
     // if (users.length > 0) {
     //     return users;
     // }
-    //console.log(users);
+    DEBUG && console.log(users);
     return users;
 };
